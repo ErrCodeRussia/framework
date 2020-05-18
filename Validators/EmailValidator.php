7ad91c1b98136256b50d9e2988d4e20ec37c0a39
pavelsklyar <ozpavel@yandex.ru>
2020-05-18 21:54:13 +0500

    Разработка валидаторов и JWT

---
+++ Validators/EmailValidator.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace base\Validators;
+
+
+class EmailValidator
+{
+    private static $pattern = '/^(?P<name>(?:"?([^"]*)"?\s)?)(?:\s+)?(?:(?P<open><?)((?P<local>.+)@(?P<domain>[^>]+))(?P<close>>?))$/i';
+
+    public static function validate($value)
+    {
+        var_dump(preg_match(self::$pattern, $value, $matches));
+
+//        var_dump($matches);
+    }
+}
